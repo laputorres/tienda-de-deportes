f01ba5a931d5628194621d1d6f0fd5424b393fa6
Abraham <lapu@pop-os.localdomain>
2021-08-07 01:26:19 -0500

    archivos creados

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Ingresar</title>
+    
+       
+</head>
+<body>
+<h1>Ingresa</h1>
+<form action="login.php" method="post">
+    <input type="text" name="email" placeholder="Ingresa tu email">
+    <input type="password" name="password" placeholder="tu contraseña">
+    <input type="submit" value="send">
+    </form>
+</body>
+</html>
